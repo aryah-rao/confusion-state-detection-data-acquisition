7deb490163fbb84c1d8f1cc30fd634b28a9f16dd
aryah-rao <rao_a1@denison.edu>
2024-05-30 11:02:24 -0400

    body_tracking.py

--- body_tracking.py
+++ body_tracking.py
@@ -29,111 +29,130 @@ import time
 import ogl_viewer.viewer as gl
 import numpy as np
 
-if __name__ == "__main__":
-
+def main():
+    # Check if the required localization file is provided
     if len(sys.argv) < 2:
         print("This sample display the fused body tracking of multiple cameras.")
         print("It needs a Localization file in input. Generate it with ZED 360.")
         print("The cameras can either be plugged to your devices, or already running on the local network.")
         exit(1)
 
+    # Read the localization file
     filepath = sys.argv[1]
     fusion_configurations = sl.read_fusion_configuration_file(filepath, sl.COORDINATE_SYSTEM.RIGHT_HANDED_Y_UP, sl.UNIT.METER)
     if len(fusion_configurations) <= 0:
         print("Invalid file.")
         exit(1)
 
+    # Initialize the camera senders dictionary
     senders = {}
     network_senders = {}
 
-    # common parameters
+    # Initialize the common parameters
     init_params = sl.InitParameters()
     init_params.coordinate_system = sl.COORDINATE_SYSTEM.RIGHT_HANDED_Y_UP
     init_params.coordinate_units = sl.UNIT.METER
     init_params.depth_mode = sl.DEPTH_MODE.ULTRA
     init_params.camera_resolution = sl.RESOLUTION.HD1080
 
+    # Set the communication parameters
     communication_parameters = sl.CommunicationParameters()
     communication_parameters.set_for_shared_memory()
 
+    # Set the positional tracking parameters
     positional_tracking_parameters = sl.PositionalTrackingParameters()
     positional_tracking_parameters.set_as_static = True
 
+    # Set the body tracking parameters
     body_tracking_parameters = sl.BodyTrackingParameters()
     body_tracking_parameters.detection_model = sl.BODY_TRACKING_MODEL.HUMAN_BODY_ACCURATE
     body_tracking_parameters.body_format = sl.BODY_FORMAT.BODY_18
     body_tracking_parameters.enable_body_fitting = False
     body_tracking_parameters.enable_tracking = False
 
+    # Open each camera based on the localization configuration
     for conf in fusion_configurations:
-        print("Try to open ZED", conf.serial_number)
+        print("Trying to open ZED", conf.serial_number)
         init_params.input = sl.InputType()
-        # network cameras are already running, or so they should
+
+        # If the camera is already running on the local network
         if conf.communication_parameters.comm_type == sl.COMM_TYPE.LOCAL_NETWORK:
             network_senders[conf.serial_number] = conf.serial_number
 
-        # local camera needs to be run form here, in the same process than the fusion
+        # If the camera is connected to the device
         else:
             init_params.input = conf.input_type
             
+            # Create a ZED camera object
             senders[conf.serial_number] = sl.Camera()
 
-            # init_params.set_from_serial_number(conf.serial_number) # taking out line to read from svo instead
-            
+            # Open the camera
             status = senders[conf.serial_number].open(init_params)
             if status != sl.ERROR_CODE.SUCCESS:
                 print("Error opening the camera", conf.serial_number, status)
                 del senders[conf.serial_number]
                 continue
 
+            # Enable the positional tracking
             status = senders[conf.serial_number].enable_positional_tracking(positional_tracking_parameters)
             if status != sl.ERROR_CODE.SUCCESS:
                 print("Error enabling the positional tracking of camera", conf.serial_number)
                 del senders[conf.serial_number]
                 continue
 
+            # Enable the body tracking
             status = senders[conf.serial_number].enable_body_tracking(body_tracking_parameters)
             if status != sl.ERROR_CODE.SUCCESS:
                 print("Error enabling the body tracking of camera", conf.serial_number)
                 del senders[conf.serial_number]
                 continue
 
+            # Start publishing the camera data
             senders[conf.serial_number].start_publishing(communication_parameters)
 
         print("Camera", conf.serial_number, "is open")
     
+    # Check if enough cameras are connected
     if len(senders) + len(network_senders) < 1:
         print("No enough cameras")
         exit(1)
 
     print("Senders started, running the fusion...")
         
+    # Initialize the fusion parameters
     init_fusion_parameters = sl.InitFusionParameters()
     init_fusion_parameters.coordinate_system = sl.COORDINATE_SYSTEM.RIGHT_HANDED_Y_UP
     init_fusion_parameters.coordinate_units = sl.UNIT.METER
     init_fusion_parameters.output_performance_metrics = False
     init_fusion_parameters.verbose = True
-    communication_parameters = sl.CommunicationParameters()
-    fusion = sl.Fusion()
-    camera_identifiers = []
 
+    # Set the communication parameters for fusion
+    communication_parameters = sl.CommunicationParameters()
+    
+    # Create a fusion object
+    fusion = sl.Fusion()
+    
+    # Initialize the fusion
     fusion.init(init_fusion_parameters)
         
     print("Cameras in this configuration : ", len(fusion_configurations))
 
-    # warmup
+    # Warm-up the camera data
     bodies = sl.Bodies()        
     for serial in senders:
         zed = senders[serial]
         if zed.grab() == sl.ERROR_CODE.SUCCESS:
             zed.retrieve_bodies(bodies)
 
+    # Subscribe each camera to the fusion
+    camera_identifiers = []
     for i in range(0, len(fusion_configurations)):
         conf = fusion_configurations[i]
         uuid = sl.CameraIdentifier()
         uuid.serial_number = conf.serial_number
         print("Subscribing to", conf.serial_number, conf.communication_parameters.comm_type)
 
+        # Subscribe the camera to the fusion
         status = fusion.subscribe(uuid, conf.communication_parameters, conf.pose)
         if status != sl.FUSION_ERROR_CODE.SUCCESS:
             print("Unable to subscribe to", uuid.serial_number, status)
@@ -141,41 +160,52 @@ if __name__ == "__main__":
             camera_identifiers.append(uuid)
             print("Subscribed.")
 
+    # Check if any camera is connected
     if len(camera_identifiers) <= 0:
         print("No camera connected.")
         exit(1)
 
+    # Enable the body tracking in the fusion
     body_tracking_fusion_params = sl.BodyTrackingFusionParameters()
     body_tracking_fusion_params.enable_tracking = True
     body_tracking_fusion_params.enable_body_fitting = False
     
     fusion.enable_body_tracking(body_tracking_fusion_params)
 
+    # Set the runtime parameters for the body tracking fusion
     rt = sl.BodyTrackingFusionRuntimeParameters()
     rt.skeleton_minimum_allowed_keypoints = 7
+    
+    # Create the OpenGL viewer
     viewer = gl.GLViewer()
     viewer.init()
 
-    # Create ZED objects filled in the main loop
+    # Create the objects to store the bodies
     bodies = sl.Bodies()
     single_bodies = [sl.Bodies]
 
+    # Main loop to retrieve and display the bodies
     while (viewer.is_available()):
         for serial in senders:
             zed = senders[serial]
             if zed.grab() == sl.ERROR_CODE.SUCCESS:
                 zed.retrieve_bodies(bodies)
 
+        # Process the fusion
         if fusion.process() == sl.FUSION_ERROR_CODE.SUCCESS:
             
-            # Retrieve detected objects
+            # Retrieve the detected bodies
             fusion.retrieve_bodies(bodies, rt)
-            # for debug, you can retrieve the data send by each camera, as well as communication and process stat just to make sure everything is okay
-            # for cam in camera_identifiers:
-            #     fusion.retrieveBodies(single_bodies, rt, cam); 
+            
+            # Update the viewer with the bodies
             viewer.update_bodies(bodies)
             
+    # Close the camera objects
     for sender in senders:
         senders[sender].close()
         
+    # Exit the viewer
     viewer.exit()
+
+if __name__ == "__main__":
+    main()
