1141c0e2d0d58438105b0d88d23de93e36d0ab96
aryah-rao <rao_a1@denison.edu>
2024-06-21 15:07:13 -0400

    updated body_tracking params to neural

--- body_tracking.py
+++ body_tracking.py
@@ -80,7 +80,7 @@ def main():
     init_params = sl.InitParameters()
     init_params.coordinate_system = sl.COORDINATE_SYSTEM.RIGHT_HANDED_Y_UP
     init_params.coordinate_units = sl.UNIT.METER
-    init_params.depth_mode = sl.DEPTH_MODE.ULTRA
+    init_params.depth_mode = sl.DEPTH_MODE.NEURAL
     init_params.camera_resolution = sl.RESOLUTION.HD1080
 
     # Set the communication parameters
